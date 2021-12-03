72519b1d17a05dda7d47ec67d1e1f7389541ff8a
vkcyan <wu.vkcyan@gmail.com>
2021-12-03 16:35:53 +0800

    chore: add project setting function

--- src/src/content/left/component/pageList/index.tsx
+++ src/src/content/left/component/pageList/index.tsx
@@ -1,8 +1,11 @@
 import { useCoreStore } from '@/stores/core';
 import { defineComponent, onMounted } from 'vue'
 import css from './index.module.scss';
-
+import { VueDraggableNext } from 'vue-draggable-next'
 export default defineComponent({
+  components: {
+    draggable: VueDraggableNext,
+  },
   setup() {
     const core = useCoreStore()
     onMounted(() => {
@@ -14,6 +17,18 @@ export default defineComponent({
     function changePageIndex(index) {
       core.acPageIndex = index
     }
+    function dragStart(evt) {
+
+    }
+    function dragEnd(evt) {
+      core.acPageIndex = evt.newIndex
+    }
+    function deletePage(index) {
+      core.pageData.splice(index, 1)
+    }
+    function insertPage(index) {
+      core.pageData.splice(index, 0, [])
+    }
     return () => (
       <div class={css.page_list}>
         <div class={css.add_page_cont}>
@@ -21,27 +36,31 @@ export default defineComponent({
         </div>
 
         <div class={css.page_main}>
-          {
-            core.pageData.map((item, index) => (
-              <div class={[css.page_item, index == core.acPageIndex ? css.active_item : '']} onClick={() => changePageIndex(index)}>
-                {/* {index == core.acPageIndex && <div class={css.page_item_active}></div>} */}
-                <div class={css.item_index}>{index + 1}</div>
-                <div class={[css.item_core, index == core.acPageIndex ? css.active_core : '']} > </div>
-                {
-                  index == core.acPageIndex && (
-                    <div class={css.item_action}>
-                      <div class={css.action_active}>1</div>
-                      <div class={css.action_active}>1</div>
-                      <div class={css.action_active}>1</div>
-                      <div class={css.action_active}>1</div>
-                      <div class={css.action_active}>1</div>
-                    </div>
-                  )
-                }
-
-              </div>
-            ))
-          }
+          <draggable v-model={[core.pageData]} animation={300} onStart={dragStart} onEnd={dragEnd}>
+            {
+              core.pageData.map((item, index) => (
+                <div class={[css.page_item, index == core.acPageIndex ? css.active_item : '']} onClick={() => changePageIndex(index)}>
+                  <div class={css.item_index}>{index + 1}</div>
+                  <div class={[css.item_core, index == core.acPageIndex ? css.active_core : '']} > </div>
+                  {
+                    index == core.acPageIndex && (
+                      <div class={css.item_action}>
+                        {/* <div class={css.action_active}>1</div>
+                        <div class={css.action_active}>1</div>
+                        <div class={css.action_active}>1</div> */}
+                        <div class={css.action_active} onClick={() => deletePage(index)}>
+                          <svg-icon class={css.svg_icon} color={'#2970f6'} name="shanchu" />
+                        </div>
+                        <div class={css.action_active} onClick={() => insertPage(index)}>
+                          <svg-icon class={css.svg_icon} color={'#2970f6'} name="plus" />
+                        </div>
+                      </div>
+                    )
+                  }
+                </div>
+              ))
+            }
+          </draggable>
         </div>
       </div>
     )
