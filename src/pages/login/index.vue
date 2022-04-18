6e73c20ff1d43bb36d94305669908d52bfc3aca8
金家乐 <1203163093@qq.com>
2022-04-18 14:50:32 +0800

    chore: 修改 登录和注册页面样式 以为登录和注册判断

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-    <el-input v-model="username" placeholder="请输入用户名"></el-input>
-    <el-input v-model="password" placeholder="请输入密码"></el-input>
-    <el-button @click="login">登录</el-button>
-    <el-button @click="registory">注册</el-button>
+  <div class="yd_login">
+    <div class="yd_name">易动v3</div>
+    <el-input class="login_input login_name" v-model="username" placeholder="请输入用户名"></el-input>
+    <el-input class="login_input login_pass" v-model="password" placeholder="请输入密码" type="password">
+    </el-input>
+    <el-button class="login_input login_lgbtn" @click="login" :disabled="!isDisabled">登录</el-button>
+    <el-button class="login_input login_resbtn" @click="registory">注册</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { auth, cloud } from '@/utils/request'
 import { ElMessage } from 'element-plus'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
+import { isEmail } from '@/utils/index'
 import { useRouter } from 'vue-router'
 import { useUserStore } from '@/store/user'
 
@@ -31,7 +34,22 @@ onMounted(() => {
   }
 })
 
+//登录是否可用
+const isDisabled = computed(() => {
+  if (username.value == '') {
+    return false
+  }
+  if (password.value == '') {
+    return false
+  }
+  return true
+})
+
 function login() {
+  if (!isEmail(username.value)) {
+    ElMessage.error('邮箱格式错误')
+    return false
+  }
   auth
     .signInWithEmailAndPassword(username.value, password.value)
     .then((res) => {
@@ -54,4 +72,42 @@ function registory() {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.yd_login {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .yd_name {
+    font-size: 28px;
+    color: #3d3d3d;
+    line-height: 39px;
+  }
+  .login_name {
+    margin-top: 43px;
+  }
+  .login_pass {
+    margin-top: 15px;
+  }
+  .login_lgbtn {
+    margin-top: 15px;
+    text-align: center;
+    background: #409eff;
+    color: #fff;
+    border-radius: 4px 4px 4px 4px;
+  }
+  .login_resbtn {
+    margin-left: 0;
+    margin-top: 15px;
+    text-align: center;
+    background: #ecf5ff;
+    color: #409eff;
+    border-radius: 4px 4px 4px 4px;
+  }
+  .login_input {
+    width: 308px;
+    height: 33px;
+  }
+}
+</style>
--- src/pages/registory/index.vue
+++ src/pages/registory/index.vue
@@ -1,17 +1,26 @@
 <template>
-  <div>
-    <el-input v-model="username" placeholder="请输入用户名"></el-input>
-    <el-input v-model="password" placeholder="请输入密码"></el-input>
-    <el-input v-model="verifyPassword" placeholder="请再次输入密码"></el-input>
-    <el-button @click="registory">注册</el-button>
-    <el-button @click="login">登录</el-button>
+  <div class="yd_registory">
+    <div class="yd_reg_name">注册成为用户</div>
+    <el-input class="reg_input reg_name" v-model="username" placeholder="请输入用户名"></el-input>
+    <el-input class="reg_input reg_password" v-model="password" placeholder="请输入密码" type="password">
+    </el-input>
+    <el-input
+      class="reg_input reg_verify"
+      v-model="verifyPassword"
+      placeholder="请再次输入密码"
+      type="password"
+    >
+    </el-input>
+    <el-button class="reg_input reg_btn" @click="registory" :disabled="!isDisabled">注册</el-button>
+    <el-button class="reg_input reg_login" @click="login">回到登录</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { auth, cloud } from '@/utils/request'
 import { ElMessage } from 'element-plus'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { isEmail } from '@/utils/index'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
@@ -19,7 +28,28 @@ const username = ref('')
 const password = ref('')
 const verifyPassword = ref('') // 再次输入密码
 
+const isDisabled = computed(() => {
+  if (username.value == '') {
+    return false
+  }
+  if (password.value == '') {
+    return false
+  }
+  if (verifyPassword.value == '') {
+    return false
+  }
+  return true
+})
+
 function registory() {
+  if (!isEmail(username.value)) {
+    ElMessage.error('邮箱格式错误')
+    return false
+  }
+  if (password.value != verifyPassword.value) {
+    ElMessage.error('两次输入的密码不一致')
+    return false
+  }
   auth
     .signUpWithEmailAndPassword(username.value, password.value)
     .then((res: any) => {
@@ -32,6 +62,7 @@ function registory() {
       ElMessage.error(err.message)
     })
 }
+
 function login() {
   router.push({
     name: 'login',
@@ -39,4 +70,40 @@ function login() {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.yd_registory {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .yd_reg_name {
+    font-size: 28px;
+    color: #3d3d3d;
+    line-height: 39px;
+  }
+  .reg_name {
+    margin-top: 30px;
+  }
+  .reg_password {
+    margin-top: 16px;
+  }
+  .reg_verify {
+    margin-top: 16px;
+  }
+  .reg_btn {
+    margin-top: 16px;
+    color: #409eff;
+    background: #ecf5ff;
+  }
+  .reg_login {
+    color: #131314;
+    margin-top: 16px;
+    margin-left: 0;
+  }
+  .reg_input {
+    width: 308px;
+    height: 33px;
+  }
+}
+</style>
