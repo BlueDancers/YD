1d0217b05507f84c0efd9e8f877f0b6f48416f81
红茶 <2467230789@qq.com>
2022-04-18 20:36:54 +0800

    chore: prepare for decoration page development

---
+++ src/pages/pageList/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>页面管理</div>
+</template>
+
+<script setup lang="ts">
+let item = {
+  img: '', // 缩略图
+  router: '', // 路由
+  createTime: '', // 创建时间
+  title: '', // 网页名称
+  _id: '', // 网页id
+}
+</script>
+
+<style scoped></style>
